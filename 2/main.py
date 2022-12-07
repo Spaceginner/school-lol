060400879128693538b3d390af1417b2857990c5
Spaceginner <97038786+Spaceginner@users.noreply.github.com>
2022-12-07 16:35:39 +0500

    fixed typo?

--- 2/main.py
+++ 2/main.py
@@ -17,30 +17,30 @@ terminal_width = os.get_terminal_size()[0]
 
 
 # Collecting data
-answer = input("Ви чоловік? (y/n)\n>>> ").lower()
+answer = input('Ви чоловік? (y/n)\n>>> ').lower()
 
 if answer in ['y', 'n']:
-    is_male = True if answer == "y" else False
+    is_male = True if answer == 'y' else False
 else:
-    print("\n\nПотрібно відповісти на запитання латинською літерою [y] (Так) або [n] (Ні).\n")
+    print('\n\nПотрібно відповісти на запитання латинською літерою [y] (Так) або [n] (Ні).\n')
 
     exit(-1)
 
 
-name = input("Ваше ім'я (у родовому відмінку)\n>>> ")
-surname = input("Ваше прізвище (у родовому відмінку)\n>>> ")
-school = input("Назва вашої школи (у родовому відмінку)\n>>> ")
-grade = input("Ваш клас\n>>> ")
-subject = input("Шкільний предмет у родовому відмінку)\n>>> ")
+name = input('Ваше ім\'я (у родовому відмінку)\n>>> ')
+surname = input('Ваше прізвище (у родовому відмінку)\n>>> ')
+school = input('Назва вашої школи (у родовому відмінку)\n>>> ')
+grade = input('Ваш клас\n>>> ')
+subject = input('Шкільний предмет у родовому відмінку)\n>>> ')
 
 
 # Outputing how the user should label his notebook
 print('\n\n')
-print(f"Зошит".center(terminal_width, ' '))
-print(f"з {subject}".center(terminal_width, ' '))
+print(f'Зошит'.center(terminal_width, ' '))
+print(f'з {subject}'.center(terminal_width, ' '))
 print(f'{"учня" if is_male else "учениці"} {grade} класу'.center(terminal_width, ' '))
-print(f"{school}".center(terminal_width, ' '))
-print(f"{surname} {name}".center(terminal_width, ' '))
+print(f'{school}'.center(terminal_width, ' '))
+print(f'{surname} {name}'.center(terminal_width, ' '))
 print('\n\n')
 
 
