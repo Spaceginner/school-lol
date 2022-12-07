5dd3c8836e81349eed589ca2c1ae978611b3f94b
Spaceginner <97038786+Spaceginner@users.noreply.github.com>
2022-12-07 15:29:12 +0500

    now supports round flower beds, and made the code look better

--- 1/main.py
+++ 1/main.py
@@ -1,31 +1,65 @@
-# This simple script will calculate what the length of the fence around a flower bed and how many flowers you need 
+# This simple script will calculate what the length of the fence around a flower bed and how many flowers you need
 
 
 import math
 import sys
 
 
+# DRY
+def exit(code: int):
+    input('\nPress ENTER to exit... ')
+    sys.exit(code)
+
+
+# Mode
+answer = input('Is your flower bed round? (y/[n])\n>>> ').lower()
+
+# Little check
+if not answer in ['y', 'n', '']:
+    print('Type only [y] or [n] or [<NOTHING>]')
+
+    exit(-1)
+
+is_round = True if answer == 'y' else False
+
+
 # Collecting data
 try:
-    length = float(input('Enter the length in meters of a flower bed: ').replace(',', '.'))
-    width  = float(input('Enter the width in meters of the flower bed: ').replace(',', '.'))
+    density = int(input('Enter how many flowers per square meter do you want?\n>>> '))
 except ValueError as e:
     print('\n\nError message: %s\n' % str(e))
-    print('Please, type only digits. (e.g. the input "one" is invalid, while "59.4" or "59,4" is valid)')
-    input('\nPress ENTER to exit... ')
-    sys.exit(-1)
+
+    print('Please, type only digits. (e.g. the input "four" is invalid, while "4" is valid)')
+
+    exit(-1)
+
+try:
+    if not is_round:
+        length = float(input('Enter the length in meters of your flower bed\n>>> ').replace(',', '.'))
+        width  = float(input('Enter the width in meters of your flower bed\n>>> ').replace(',', '.'))
+    else:
+        diameter = float(input('Enter the diameter in meters of your flower bed\n>>> '.replace(',', '.')))
+except ValueError as e:
+    print('\n\nError message: %s\n' % str(e))
+
+    print('Please, type only digits. (e.g. the input "fifty-nine point four" is invalid, while "59.4" or "59,4" is valid)')
+
+    exit(-1)
 
 
 # Calculation
-perimiter = 2 * (length + width)
-square = length * width
+if not is_round:
+    perimiter = 2 * (length + width)
+    square = length * width
+else:
+    perimiter = math.pi * diameter
+    square = math.pi * (diameter * 0.5) ** 2
 
-flowers_amount = math.floor(square * 4)
+flowers_amount = math.floor(square * density)
 
 
 # Printing out the info and exiting
 print('The needed length of the fence is %s meters' % str(round(perimiter, 2)))
-print('Also, you need %d flowers' % flowers_amount)
+print('And, you need %d flowers' % flowers_amount)
 
-input('\nPress ENTER to exit... ')
-sys.exit(0)
+exit(0)
